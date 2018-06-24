dbc69d2aa861b3b67f786b6a514a186542af619f
andersS91 <anders-soerensen@hotmail.com>
2018-06-24 21:46:00 +0200

    Setup

---
+++ Breakout/Breakout/Main.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm/glm.hpp>
+#include <glm/glm/gtc/matrix_transform.hpp>
+#include <glm/glm/gtc/type_ptr.hpp>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include <iostream>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <random>
